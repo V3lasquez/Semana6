cb4e8c9759e9a0f5ddaab0fed9db64cd7ca45e0b
V3lasquez <noe.vc1782@gmail.com>
2023-10-07 12:54:12 -0500

    Agregar archivos de proyecto.

---
+++ Semana6/Semana6/ButtonDemo.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ButtonDemo : ContentPage
+    {
+        public ButtonDemo()
+        {
+            InitializeComponent();
+        }
+        async void OnButtonClicked(object sebder, EventArgs a)
+        {
+            await Label.RelRotateTo(360, 1000);
+        }
+    }
+}
---
+++ Semana6/Semana6/ImageResourceExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace Semana6
+{
+    [Preserve(AllMembers = true)]
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public String Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+            var imageSource = ImageSource.FromResource(Source,
+                typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
